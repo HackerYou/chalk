125e6510c64ff42b9f54a9b6e8ce8fda21ceab1c
Kristen Spencer <kristen@kristenspencer.ca>
2015-11-16 13:56:23 -0500

    added routing and components

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router, Route, Link } from 'react-router';
+import { Router, Route, Link, Navigation } from 'react-router';
 
+let createBrowserHistory = require('history/lib/createBrowserHistory');
+
+import Login from './login/index.jsx';
 import Dashboard from './dashboard/index.jsx';
+import Classroom from './classroom/index.jsx';
+import Lesson from './lesson/index.jsx';
+import Exercises from './exercises/index.jsx';
+import Topics from './topics/index.jsx';
+import Footer from './footer/index.jsx';
+import Media from './media/index.jsx';
+import Instructors from './instructors/index.jsx';
 
 let App = React.createClass({
 	render() {
@@ -14,25 +24,41 @@ let App = React.createClass({
 							<Link to="/">Home</Link>
 						</li>
 						<li>
-							<Link to="dashboard">Dasboard Link</Link>
+							<Link to="dashboard">Dashboard Link</Link>
+						</li>
+						<li>
+							<Link to="classroom">Classroom Link</Link>
+						</li>
+						<li>
+							<Link to="exercises">Exercises Link</Link>
+						</li>
+						<li>
+							<Link to="lesson">Lesson Link</Link>
+						</li>
+						<li>
+							<Link to="topic">Topic Link</Link>
 						</li>
 					</ul>
 				</header>
+
 				<section>
-					{this.props.children || <h1>HI</h1>}
+					{this.props.children || <Login />}
 				</section>
-				<footer className="mainFooter">
-					<p>Footer Stuff</p>
-				</footer>
+				<Footer />
 			</div>
 		);
 	}
 });
 
+
 ReactDom.render(
-	(<Router>
-		<Route path='/' component={App}>
+	(<Router history={createBrowserHistory()}>
+			<Route path='/' component={App}>
 			<Route path='/dashboard' component={Dashboard} />
+			<Route path='/classroom' component={Classroom} />
+			<Route path='/lesson' component={Lesson} />
+			<Route path='/topics' component={Topics} />
+			<Route path='/exercises' component={Exercises} />
 		</Route>
 	</Router>)
 	, document.getElementById('app'));
