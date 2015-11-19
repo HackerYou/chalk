504893f5e53978cf3460595dd96770eeb2a16716
Kristen Spencer <kristen@kristenspencer.ca>
2015-11-19 17:12:12 -0500

    added lesson view with exercise and topic components

---
+++ app/components/exercise/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default React.createClass({
+	displayName: 'Exercise',
+	render() {
+		return (
+			<p className="red"><i className="chalk-download"></i>Download{this.props.details.title}</p>
+		)
+	}
+});
--- app/components/sample-data.js
+++ app/components/sample-data.js
@@ -140,5 +140,185 @@ module.exports = {
       "created_at": 1447868967553,
       "_id": "564cba27c48acc6c248af1e1",
       "students": 27
-    }
+    },
+    "lesson": {
+        "topic": [
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "dolor aliqua enim velit cillum excepteur id culpa tempor consequat proident fugiat velit labore ut aliqua cupidatat magna in culpa qui tempor dolor consectetur cillum cupidatat magna laborum sit qui reprehenderit aliquip nostrud ad culpa sunt qui ullamco dolore deserunt consequat aute sunt do nostrud anim non nisi fugiat qui",
+            "title": "deserunt culpa",
+            "_id": "564e352bc8f7e663b36f2945"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "eiusmod Lorem aliquip mollit ullamco ea culpa duis nostrud tempor ea dolor sint ut proident in est qui cupidatat reprehenderit veniam irure ipsum officia pariatur officia veniam labore veniam minim incididunt qui do proident aliqua adipisicing tempor adipisicing ex consequat velit pariatur irure reprehenderit aute fugiat ea enim enim do",
+            "title": "voluptate sunt",
+            "_id": "564e352b8602693b007e52e8"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "voluptate laborum sunt amet est excepteur et irure cupidatat velit elit eu velit mollit Lorem cupidatat voluptate cillum excepteur fugiat reprehenderit enim Lorem velit dolore et in nostrud ipsum anim laborum velit pariatur dolore mollit ipsum nostrud cupidatat aliqua duis cupidatat dolor ut cillum excepteur voluptate culpa voluptate labore nostrud",
+            "title": "excepteur minim",
+            "_id": "564e352bc0f4bf52a980df67"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "ut id adipisicing adipisicing in aute pariatur veniam exercitation nulla mollit incididunt minim mollit aute culpa pariatur non commodo nostrud dolore Lorem dolor ea consequat occaecat fugiat pariatur non culpa dolore tempor sint ea ad nulla pariatur est tempor aute labore laborum enim cillum aute labore esse ex excepteur ex",
+            "title": "voluptate consequat",
+            "_id": "564e352b08ed49bd2557fbb5"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "officia nisi culpa non minim occaecat elit adipisicing tempor sint duis non duis magna adipisicing consectetur exercitation laboris dolor esse commodo eiusmod sunt esse cupidatat consectetur culpa exercitation deserunt nisi mollit reprehenderit ipsum laborum labore eu labore ea reprehenderit sunt occaecat minim sit dolor cillum id ut cupidatat consectetur nisi",
+            "title": "officia ea",
+            "_id": "564e352be8cc014d1d17f3ad"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "aliqua ea non exercitation id consequat sit ad et dolore voluptate consequat sint anim exercitation dolor eiusmod aliquip irure veniam irure incididunt esse sunt velit qui velit laboris velit irure consequat dolore veniam amet consectetur et ipsum mollit laborum sit irure occaecat tempor aliqua qui deserunt ut pariatur sunt mollit",
+            "title": "cupidatat incididunt",
+            "_id": "564e352baa378e1c0b24e1cb"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "cupidatat esse in exercitation Lorem sit aute eiusmod ipsum reprehenderit eu do qui aute incididunt quis quis veniam commodo exercitation nulla non dolor sunt irure eu commodo officia nulla fugiat id voluptate labore tempor nisi fugiat laborum sunt aliqua nisi excepteur Lorem ea ut culpa cupidatat elit eu ut laborum",
+            "title": "id consequat",
+            "_id": "564e352b2688ef8408830995"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "sint culpa et sint adipisicing eiusmod sint mollit sunt id quis aute consectetur occaecat anim aliquip eu eu ipsum culpa commodo enim reprehenderit eiusmod irure nisi tempor exercitation proident sint consequat dolor velit qui exercitation id veniam irure ipsum voluptate ex id dolore dolor officia duis ut cillum sunt aliqua",
+            "title": "in dolore",
+            "_id": "564e352bcaf572c34015f5da"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "esse excepteur anim culpa ea labore id amet culpa nulla ullamco incididunt commodo in culpa esse aute proident nostrud velit enim sit veniam qui laboris duis commodo qui in duis elit minim veniam non officia commodo cillum duis consectetur irure irure commodo sunt Lorem commodo commodo adipisicing id consequat excepteur",
+            "title": "exercitation anim",
+            "_id": "564e352b489b2b341dfcd04e"
+          },
+          {
+            "exercises": [
+              {
+                "body": "veniam reprehenderit amet do sint esse est culpa aute aliquip officia magna aute officia mollit proident duis occaecat anim nisi",
+                "title": "velit proident",
+                "_id": "564e352b60bd87f467935273"
+              },
+              {
+                "body": "aute mollit ex amet veniam irure aliqua esse tempor veniam eiusmod laboris voluptate fugiat excepteur laborum et esse amet velit",
+                "title": "ut sunt",
+                "_id": "564e352b3fb9315ee602f35d"
+              }
+            ],
+            "body": "et ut ex culpa eu ex cupidatat labore reprehenderit cupidatat anim enim non Lorem ipsum incididunt ut officia pariatur ipsum officia labore qui enim proident ea dolor eu proident sunt sint pariatur ex deserunt quis pariatur voluptate id quis consectetur mollit aliqua pariatur commodo ad officia fugiat dolor nisi aute",
+            "title": "occaecat officia",
+            "_id": "564e352bb8d2cf43d29ff349"
+          }
+        ],
+        "title": "Colour, Background and Attributes",
+        "created_by": "564e352baca6947771d3b4e1",
+        "description": "qui excepteur cillum ea reprehenderit veniam minim aute ad fugiat excepteur minim eu laborum velit",
+        "body": "exercitation in proident est pariatur eiusmod ad velit aliquip eiusmod commodo enim dolor esse exercitation occaecat do enim velit nulla fugiat Lorem sint irure et cillum sit voluptate exercitation qui anim ad duis ea proident",
+        "created_at": 1447965995383,
+        "_id": "564e352ba76ddc1ba5ed8591"
+      }
 }
