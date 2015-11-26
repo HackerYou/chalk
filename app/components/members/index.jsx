d9a7186822ca69694b3d22a25b8d7ae82fde0b07
Kristen Spencer <kristen@kristenspencer.ca>
2015-11-26 12:08:04 -0500

    working on members page

---
+++ app/components/members/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default React.createClass({
+	displayName: 'Members',
+	getInitialState(){
+		return {
+			members: []
+		}
+	},
+	componentWillMount(){
+		let data = require('../sample-data.js');
+		this.setState({
+			members: data.members
+		});
+	},
+	renderMembers(key, index){
+		return <li key={index}>
+							<p><strong>{this.state.members[index].firstName + ' ' + this.state.members[index].lastName}</strong></p>
+							<p>{this.state.members[index].email}</p>
+						</li>
+	},
+	render() {
+		return (
+			<div>
+				<Link to="dashboard" className="linkBtn"><button className="primary"><i className="chalk-home"></i>back to dashboard</button></Link>
+				<h1>Manage Members</h1>
+				<form action="" className="card">
+					<label htmlFor="search">Search by name or email</label>
+					<input type="text" id="search"/>
+				</form>
+				<ul>
+					{this.state.members.map(this.renderMembers)}
+				</ul>
+			</div>
+			)
+	}
+});
