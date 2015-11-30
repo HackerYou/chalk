47766f7dfc89d6ab11555d6175474f85f70c67c5
Ryan Christiani <ryan@ryanchristiani.com>
2015-11-30 16:57:15 -0500

    api url should point to live api

--- app/services/config.jsx
+++ app/services/config.jsx
@@ -1,6 +1,6 @@
 
-// let url = 'http://notes-api.hackeryou.com/v1';
-let url = 'http://localhost:3200/v1';
+let url = 'http://notes-api.hackeryou.com/v1';
+// let url = 'http://localhost:3200/v1';
 let appName = 'chalk';
 
 export default {
