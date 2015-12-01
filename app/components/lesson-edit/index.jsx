f98f2c403e5a93e80715986b773025522291240e
Kristen Spencer <kristen@kristenspencer.ca>
2015-11-30 21:36:02 -0500

    Chapter 45: Lets make a lesson

--- app/components/lesson-edit/index.jsx
+++ app/components/lesson-edit/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import Lesson from '../lesson/index.jsx';
 import Modal from '../modal/index.jsx';
 import lessonData from '../../services/lesson.jsx';
+import topicsData from '../../services/topic.jsx';
 
 export default React.createClass({
 	displayName: 'EditLesson',
@@ -10,14 +11,20 @@ export default React.createClass({
 		return {
 			isModalOpen: false,
 			lesson: [],
+			lessonTopics: [],
 			topics: []
 		}
 	},
 	componentWillMount(){
-		console.log(this.props);	
 		lessonData.getLessonById(this.props.params.lessonId).then(res => {
 			this.setState({
-				lesson: res.lesson
+				lesson: res.lesson,
+				lessonTopics: res.lesson.topics
+			});
+		});
+		topicsData.getTopics().then(res=>{
+			this.setState({
+				topics: res.topic
 			});
 		});
 	},
@@ -26,6 +33,39 @@ export default React.createClass({
 	},
 	closeModal(){
 		this.setState({isModalOpen: false});
+	},
+	renderTopics(key, index){
+		return <li key={index}>
+						<button onClick={this.addTopic} className="primary" id={this.state.topics[index]._id}>{this.state.topics[index].title}</button>
+					</li>
+	},
+	addTopic(e){
+		e.preventDefault();
+		lessonData.addTopicToLesson(this.props.params.lessonId, e.target.id, {
+			'topics': e.target.id
+		}).then(res=>{
+			this.closeModal();
+			topicsData.getTopicById(e.target.id).then(res=>{
+				let updatedTopics = this.state.lessonTopics.slice();
+				updatedTopics.push(res.topic);
+				this.setState({lessonTopics: updatedTopics});
+			});
+		});
+
+	},
+	deleteTopic(index){
+		lessonData.deleteTopicFromLesson(this.props.params.lessonId, this.state.lessonTopics[index]._id).then(res=>{
+			let updatedLessons = this.state.lessonTopics.slice();
+			updatedLessons.splice(index, 1);
+			this.setState({lessonTopics: updatedLessons});
+		});
+	},
+	displayTopics(key, index){
+		return <div key={index} className='topic'>
+						<h3>{this.state.lessonTopics[index].title}</h3>
+						<p>{this.state.lessonTopics[index].body}</p>
+						<button data-id={this.state.lessonTopics[index]._id} onClick={this.deleteTopic.bind(this, index)}className="error">Delete Topic</button>					
+						</div>
 	},
 	render() {
 		return (
@@ -39,6 +79,8 @@ export default React.createClass({
 					<button className="error"><i className="chalk-close"></i>Cancel</button>
 				</form>
 				<div className="card">
+						<h2>{this.state.lesson.title}</h2>
+						<div>{(this.state.lessonTopics).map(this.displayTopics)}}</div>
 						<div onClick={this.openModal}><h3><i className="chalk-add"></i>Add Topic</h3></div>
 						<Modal isOpen={this.state.isModalOpen} transitionName='modal-animation'>
 							<i className="chalk-close" onClick={this.closeModal}></i>
@@ -46,6 +88,9 @@ export default React.createClass({
 								<h2>Add Topic</h2>
 								<h3>Search By Topic Name</h3>
 								<input type="text" placeholder='eg. Floats'/>
+								<ul>
+									{(this.state.topics).map(this.renderTopics)}
+								</ul>
 								<h4>or</h4>
 								<h3>Add your own content</h3>
 								<textarea name="content" id="" cols="30" rows="10"></textarea>
