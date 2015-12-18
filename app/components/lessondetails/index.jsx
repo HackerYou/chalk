c7c2971b72a8cbeab98e2d228e0f95433e4cc6d0
Ryan Christiani <ryan@ryanchristiani.com>
2015-12-18 12:51:09 -0500

    Favs

--- app/components/lessondetails/index.jsx
+++ app/components/lessondetails/index.jsx
@@ -6,25 +6,24 @@ export default React.createClass({
 	displayName: 'LessonDetails',
 	getInitialState() {
 		return {
-			star: false
+			star: ''
 		}
 	},
 	starLesson() {
-		if(!this.state.star) {
-			userData.favoriteLesson(this.props.courseId,this.props.details._id).then((res) => {
+		if(!this.state.star) {	
+			userData.favoriteLesson(this.props.classroomId,this.props.details._id).then((res) => {
 				this.setState({
 					star: !this.state.star
 				});
 			});
 		}
 		else {
-			userData.unFavoriteLesson(this.props.courseId,this.props.details._id).then((res) => {
+			userData.unFavoriteLesson(this.props.classroomId,this.props.details._id).then((res) => {
 				this.setState({
 					star: !this.state.star
 				});
 			});
-		}
-		//Favorite or remove fav
+		}	
 	},
 	render() {
 		let editLink = <Link to={`/lesson/${this.props.details._id}/${this.props.classroomId}/edit`}>edit</Link>;
@@ -38,7 +37,7 @@ export default React.createClass({
 						<Link to={`/lesson/${this.props.details._id}/${this.props.classroomId}`}>view</Link>|
 						{this.props.canEdit || this.props.isTemplate ? editLink : null}
 					</span>
-					<i className={this.state.star ? 'chalk-star redLight' : 'chalk-star'} onClick={this.starLesson}></i>
+					<i className={this.state.star || this.props.star ? 'chalk-star redLight' : 'chalk-star'} onClick={this.starLesson}></i>
 				</div>
 			</li>
 		)
