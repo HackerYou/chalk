9f53fa6796533952e29cae9fb18a250badc60a13
Sylvia <sylvia.nguyen12@gmail.com>
2017-01-27 12:07:36 -0500

    student progress is showing on approprate course now

--- app/components/classroom/results-card.jsx
+++ app/components/classroom/results-card.jsx
@@ -4,26 +4,32 @@ import { Link , History} from 'react-router';
 export default function(props) {
 	const testRes = props.studentInfo.tests;
 	const answers = props.studentInfo.test_results;
-	console.log("answers", answers);
-	
+	const courseTestIds = props.course.tests.map((test) => test._id);
+	let counter = 0;
 	return (
 		<div className="classCard">
 			<article className="card">
 				<h3>{props.studentInfo.firstName} {props.studentInfo.lastName}</h3>
 				<div className="progressCard">
 					<ul className="testProgress testProgress__circles">
-						{answers !== "" && answers.map((test, i) => {
-							const isCorrect = test.answers.filter((item) => {
-								return item.correct === true
-							})
-							const passGrade = test.answers.length * 0.6;
-							i = i + 1;
-							return <li className={isCorrect.length >= passGrade ? "testProgress__circles--pass" : "testProgress__circles--fail"} key={i}>{i}</li>
-						})}
+							{answers.map((test, i) => {
+								if(courseTestIds.includes(test.id)) {
+									counter++;
+									//only show results for test that are on this course 
+									const isCorrect = test.answers.filter((item) => {
+										return item.correct === true
+									});
+									const passGrade = test.answers.length * 0.6;
+									return <li className={isCorrect.length >= passGrade ? "testProgress__circles--pass" : "testProgress__circles--fail"} key={`testcard-${i}`}>{counter}</li>
+								}
+
+							})}
 					</ul>
 					<ul>
 						{props.studentInfo.tests.map((test, i) => {
-							return <li key={i}>{test.title}</li>
+							if(courseTestIds.includes(test._id)) {
+								return <li key={`testtitle-${i}`}>{test.title}</li>
+							}
 						})}
 					</ul>
 				</div>
--- app/components/classroom/test-results.jsx
+++ app/components/classroom/test-results.jsx
@@ -6,6 +6,10 @@ import userData from '../../services/user.jsx';
 import TestData from '../../services/tests.jsx';
 import TestCards from '../classroom/results-card.jsx';
 
+function fold(arr) {
+	return arr[0];
+}
+
 export default React.createClass({
 	displayName: 'TestResults',
 	mixins: [AuthMixin,History],
@@ -34,12 +38,25 @@ export default React.createClass({
 	},
 	renderCards() {
 		return (
-				this.state.members.map((res) => {
-					console.log("user", res.user);
-					return <TestCards studentInfo={res.user} />
-					
-				})
-			)
+			this.state.members.map((res) => {
+				const courseId = this.props.params.courseId;
+				const course = fold(
+					res.user.courses.filter((course) => {
+						return	course._id === courseId
+					})
+					.map(course => {
+						course.tests = course.tests.map(test => {
+							return { _id: test} 
+						});
+						return course;
+					})
+				);
+				console.log(course);
+				//Find the course we are currenty on, and only pass those tests to the testCard 
+				return <TestCards studentInfo={res.user} course={course} />
+				
+			})
+		)
 	},
 	render() {
 		return (
