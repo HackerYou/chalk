a50eac0448f2f6c1b1ef91ac0ec924805f45d1de
Ryan Christiani <ryan@ryanchristiani.com>
2017-02-13 20:49:29 -0500

    Refactored testcard to add score

--- app/components/classroom/results-card.jsx
+++ app/components/classroom/results-card.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import { Link , History} from 'react-router';
 
+function correctAnswers(test) {
+	return test.answers.filter((item) => {
+		return item.correct === true || item.correct.numFailedTestSuites === 0
+	});
+}
+
 export default function TestCard(props) {
 	const testRes = props.studentInfo.tests;
 	const answers = [];
+	const correct = {};
 	for(let key in props.studentInfo.test_results) {
 		answers.push(props.studentInfo.test_results[key]);
 	}
-	const courseTestIds = props.course.tests.map((test) => test._id);
-	let counter = 0;
 	return (
 		<div className="classCard">
 			<article className="card">
 				<h3 className="studentName">{props.studentInfo.firstName} {props.studentInfo.lastName}</h3>
 				<div className="progressCard">
 					<ul className="testProgress testProgress__circles">
-						{answers !== "" && answers.map((test, i) => {
-							const isCorrect = test.answers.filter((item) => {
-								return item.correct === true || item.correct.numFailedTestSuites === 0
-							})
+						{answers.map((test, i) => {
+							const isCorrect = correctAnswers(test);
+							correct[i] = isCorrect;
 							const passGrade = test.answers.length * 0.6;
-							i = i + 1;
-							return <li className={isCorrect.length >= passGrade ? "testProgress__circles--pass" : "testProgress__circles--fail"} key={`result-${i}`}>{i}</li>
+							return <li className={isCorrect.length >= passGrade ? "testProgress__circles--pass" : "testProgress__circles--fail"} key={`result-${i + 1}`}>{i + 1}</li>
 						})}
 					</ul>
 					<ul>
 						{props.studentInfo.tests.map((test, i) => {
-							return <li key={`resulttitle-${i}`}>{test.title}</li>
+							return <li key={`resulttitle-${i}`}>{test.title} {correct[i].length}/{test.questions.length}</li>
 						})}
 					</ul>
 				</div>
