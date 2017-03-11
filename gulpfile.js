03641e5b9a55bfa8c79994a05b3be2526f2b601e
Simon Bloom <swbloom@gmail.com>
2017-03-10 21:19:43 -0500

    adds watchify

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const gulp = require('gulp'),
 		reload = browserSync.reload,
 		$ = require('gulp-load-plugins')(),
 		scss = require("postcss-scss"),
+		watchify = require("watchify"),
 		historyApiFallback = require('connect-history-api-fallback');
 
 // Define paths
@@ -29,6 +30,8 @@ const lint = [
 	// require('stylelint')(),
 	require('postcss-reporter')({ clearMessages: true, throwError: true })
 ];
+
+
 
 // Setup PostCSS Plugins
 const postcss = [
@@ -110,16 +113,50 @@ gulp.task('bs-guide', function () {
 	})
 });
 
+
+gulp.task('browserify', () => {
+	const bundler = watchify(browserify(paths.jsx, { debug: true, cache: {} }))
+		.transform(babelify, {
+			presets: ['es2015', 'react'],
+			plugins: ['transform-object-rest-spread']
+		})
+
+	const rebundle = function() {
+		return bundler.bundle()
+			.on('error', (err) => {
+				console.log(err);
+				this.emit('end');
+			})
+			.pipe(source('app.min.js'))
+			.pipe(buffer())
+			.pipe($.sourcemaps.init({
+				loadMaps: true
+			}))
+			// .pipe($.uglify())
+			.pipe($.sourcemaps.write('.'))
+			.pipe(gulp.dest('app/components'))
+			.pipe(reload({stream:true}));
+
+	}
+
+	bundler.on('update', function() {
+		console.log('-> bundling');
+		rebundle();
+	});
+
+	rebundle();
+});
+
 gulp.task('js', function() {
-	browserify(paths.jsx)
+	return browserify(paths.jsx)
 		.transform(babelify,{presets: ["es2015", "react"], plugins: ["transform-object-rest-spread"]})
 		.bundle().on('error', $.notify.onError({
       title: "JSX Error",
-      message: "<%= error.message %>"
+      message: "<%= error.message %>",
     }))
 	.pipe(source('app.min.js'))
     .pipe(buffer())
-    .pipe($.sourcemaps.init({loadMaps: true}))
+    // .pipe($.sourcemaps.init({loadMaps: true}))
     .pipe($.uglify())
     .pipe($.sourcemaps.write('.'))
 	.pipe(gulp.dest('app/components'));
@@ -136,9 +173,9 @@ gulp.task('bs-client', function () {
 
 gulp.task('build', ['js','styles']);
 
-gulp.task('default', ['styles', 'js','bs-client'], () => {
-	gulp.watch('app/**/*.jsx',['js']);
-	gulp.watch('app/components/app.min.js', reload);
+gulp.task('default', ['styles', 'browserify','bs-client'], () => {
+	// gulp.watch('app/**/*.jsx',['browserify']);
+	// gulp.watch('app/components/app.min.js', reload);
 	gulp.watch(paths.srcCSS + '**/*.scss', ['styles']);
 });
 
