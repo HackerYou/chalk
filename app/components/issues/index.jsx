ad12d10f1bbf853f7ac24adc575d172d631a638c
Ryan Christiani <ryan@ryanchristiani.com>
2017-04-04 16:22:57 -0400

    Adding issues

---
+++ app/components/issues/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {  Link,  History } from 'react-router';
+import AuthMixin from '../../services/authMixin.jsx';
+import IssuesCard from './card.jsx';
+import issuesData from '../../services/issues.jsx';
+
+export default React.createClass({
+	displayName: 'Issues',
+	mixins: [AuthMixin, History],
+	getInitialState(){
+		return {
+			issues: []
+		}
+	},
+	componentDidMount() {
+		issuesData.getAllIssues()
+			.then((data) => {
+				this.setState({
+					issues: data.issues
+				})
+			})
+	},
+	render() {
+		return (
+			<div className="content">
+				<h1></h1>
+				<section className="dashWrap">
+					{this.state.issues.map((item) => {
+						return <IssuesCard issue={item} />
+					})}
+				</section>
+			</div>
+		)
+	}
+})
